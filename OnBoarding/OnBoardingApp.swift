49fae748624cd0e0ad410d694033bb405a25c2f2
tharmJaa <timeruchu0808@gmail.com>
2023-03-22 15:12:21 +0700

    Initial Commit

---
+++ OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by Time Ruchutrakool on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
